38c150b9497bd5cdee81369632ac00000678519c
Dom056 <dominicdawson99@gmail.com>
2026-04-05 13:54:25 +0100

    created simple navbar

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,21 @@
+import "./navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navLeft">
+        <div className="navLinks">
+          <a href="#">Home</a>
+          <a href="#">Transactions</a>
+          <a href="#">Spending Insights</a>
+          <a href="#">Credit Score</a>
+        </div>
+      </div>
+
+      <div className="navIcons">
+        <span role="img" aria-label="notifications">🔔</span>
+        <span role="img" aria-label="profile">👤</span>
+      </div>
+    </nav>
+  );
+}
