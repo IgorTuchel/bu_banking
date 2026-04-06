aa7c68204985b6bca27b04d19b109146b7653e98
DarrellClarke <darrell.clarke98@gmail.com>
2026-04-06 19:53:30 +0100

    Enhance Navbar Branding

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -7,7 +7,12 @@ export default function Navbar() {
       <div className="navLeft">
         <div className="navBrand">
           <div className="navLogoWrapper">
-            <img src={logo} alt="MyBank logo" className="navLogo" />
+            <img src={logo} alt="Aurix logo" className="navLogo" />
+          </div>
+
+          <div className="navBrandTextGroup">
+            <div className="navBrandText">Aurix</div>
+            <div className="navBrandTagline">Private Banking</div>
           </div>
         </div>
 
