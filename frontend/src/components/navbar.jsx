e008a48d92e9bc5756fb6218fbe1632acf4ead4c
DarrellClarke <darrell.clarke98@gmail.com>
2026-04-18 13:42:23 +0100

    started navbar redesign

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -4,35 +4,50 @@ import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="navLeft">
-        <button className="menuButton" aria-label="Open menu">
-          ☰
-        </button>
+    <header className="navbarFrame">
+      <div className="navbarTopGold" />
+      <div className="navbarTopGreen" />
 
-        <div className="navBrand">
-          <div className="navLogoWrapper">
-            <img src={logo} alt="Aurix logo" className="navLogo" />
-          </div>
+      <nav className="navbarLayout">
+        <div className="navbarBrandBlock">
+          <div className="navBrand">
+            <div className="navLogoWrapper">
+              <img src={logo} alt="Aurix logo" className="navLogo" />
+            </div>
 
-          <div className="navBrandTextGroup">
-            <div className="navBrandText">Aurix</div>
-            <div className="navBrandTagline">Private Banking</div>
+            <div className="navBrandTextGroup">
+              <div className="navBrandText">Aurix</div>
+              <div className="navBrandTagline">Private Banking</div>
+            </div>
           </div>
         </div>
 
-        <div className="navLinks">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/transactions">Transactions</NavLink>
-          <NavLink to="/rewards">Rewards</NavLink>
-          <NavLink to="/support">Help &amp; Support</NavLink>
-        </div>
-      </div>
+        <div className="navbarCenterZone">
+          <div className="navCapsuleGroup">
+            <button className="navCapsuleMenuButton" aria-label="Open menu">
+              ☰
+            </button>
 
-      <div className="navIcons">
-        <span role="img" aria-label="notifications">🔔</span>
-        <span role="img" aria-label="profile">👤</span>
-      </div>
-    </nav>
+            <div className="navLinksCapsule">
+              <NavLink to="/">Home</NavLink>
+              <NavLink to="/transactions">Transactions</NavLink>
+              <NavLink to="/rewards">Rewards</NavLink>
+              <NavLink to="/support">Help &amp; Support</NavLink>
+            </div>
+          </div>
+        </div>
+
+        <div className="navbarRightBlock">
+          <div className="navbarRight">
+            <button className="navIconButton" aria-label="Notifications">
+              🔔
+            </button>
+            <button className="navIconButton" aria-label="Profile">
+              👤
+            </button>
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 }
